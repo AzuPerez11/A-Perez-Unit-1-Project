71003dccf83eb05c3d967114581bc2cd611e2e5f
AzuPerez11 <45808783+AzuPerez11@users.noreply.github.com>
2019-01-31 23:01:22 -0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,19 +4,8 @@ FSJS project 1 - A Random Quote Generator
 ******************************************/
 
 // Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
-var quoteBox = document.getElementById("quote-box");
-quoteBoxDiv.innerHTML = html;
-console.log(passingQuote.quote);
-};
-
 /*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-
-  Recommended: 
-    - Add at least one `year` and/or `citation` property to at least one 
-      quote object.
+ These are the quotes that will appear on the screen, one at a time, randomly. 
 ***/
 var quotes = [
   {quote: "A-a-aye, I'm on vacation every single day 'cause I love my occupation. A-a-aye. I'm on vacation, If you don't like your life, then you should go and change it.",
@@ -41,19 +30,13 @@ var quotes = [
     citation: "Ms. Jackson"},
    ];
 
-
-
 /***
-  Create the `getRandomQuote` function to:
-   - generate a random number 
-   - use the random number to `return` a random quote object from the 
-     `quotes` array.
+  The "getRandomQuote" function generates a random quote from the array of objects above to display on the screen.
 ***/
-function getRandomQuote(quotes){
-  var randomQuote = Math.floor(Math.random(quotes[0]) * 7);
-  return randomQuote;
+function getRandomQuote(bananas){
+  var randomNumber = Math.floor(Math.random() * 7);
+  return bananas[randomNumber]
 };
-
 
 /***
   Create the `printQuote` function to: 
@@ -65,30 +48,17 @@ function getRandomQuote(quotes){
    - set the `innerHTML` of the `quote-box` div to the HTML string. 
 ***/
 function printQuote(){
-  var passingQuotes = getRandomQuote(quotes);
-  var html = "
-  <p class="quote"> [quotes.quote] </p>
-  <p class="source"> [quotes.source]
-</p>"
+  var passingQuote = getRandomQuote(quotes);
+  var html = "";
+  html += '<p class="quote">' + passingQuote.quote + "</p>";
+  html += '<p class="source">' + passingQuote.source + "</p>";
+if (typeof passingQuote.citation !== "undefined"){
+  html += '<span class = "citation">' + passingQuote.citation + "</span>";
+} 
 
 var quoteBox = document.getElementById("quote-box");
-quoteBoxDiv.innerHTML = html;
+quoteBox.innerHTML = html;
 console.log(passingQuote.quote);
-};
-
-function showCitation(passingQuotes){
-  var other = "";var quoteBox = document.getElementById("quote-box");
-  quoteBoxDiv.innerHTML = html;
-  console.log(passingQuote.quote);
-  };var quoteBox = document.getElementById("quote-box");
-quoteBoxDiv.innerHTML = html;
-console.log(passingQuote.quote);
-};
-  if(passingQuotes.citation){
-     other =  "<span class="citation"> [quotes.citation] </span>";
-  };
-  return other;
-};
 /***
   When the "Show another quote" button is clicked, the event listener 
   below will be triggered, and it will call, or "invoke", the `printQuote` 
